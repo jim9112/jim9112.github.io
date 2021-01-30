b6f1decf29c80a38748264b7a51d6db568bb28dc
jim9112 <jim9112@gmail.com>
2021-01-30 14:07:58 -0500

    add app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+const hamburgerMenuIcon = document.querySelector('.hamburger');
+
+const handleClick = () => {
+    console.log('test');
+}
+
+hamburgerMenuIcon.addEventListener('click', handleClick);
